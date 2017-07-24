03fe517021ef0fe14e8b307527521b2337a9df23
Jari Zwarts <jarizw@gmail.com>
2017-07-24 20:50:05 +0200

    Only check flow at runtime (cleaner code, worse user experience 🙈)

--- questions/flow.js
+++ questions/flow.js
@@ -9,37 +9,21 @@ const get = promisify(cmd.get, {
 });
 
 export default {
-    type: 'input',
+    type: 'confirm',
     name: 'flow',
     message: 'Use flow? (http://flow.org)',
-    default: 'Yes',
-    validate: async answer => {
-        if(answer) {
-            // we're gonna use flow, so check if flow is actually installed...
-            try {
-                await get('which flow');
-            } catch(ex) {
-                return 'Could not find flow executable (did you run \'npm install -g flow-bin\'?)';
-            }
-        }
-        return true;
-    },
-    filter: answer => {
-        // can't use type: confirm because inquirer won't call validate in that case :(
-        switch(answer.toLowerCase()) {
-            case 'y':
-            case 'yes':
-                return true;
-            case 'no':
-            case 'n':
-                return false;
-            default:
-                throw new Error("Invalid input");
-        }
-    }
 };
 
 export const execute = async (answer, { appname }) => {
+    if(answer) {
+        // we're gonna use flow, so check if flow is actually installed...
+        try {
+            await get('which flow');
+        } catch(ex) {
+            throw new Error('Could not find flow executable (did you run \'npm install -g flow-bin\'?');
+        }
+    }
+    
     await run('flow init', {
         cwd: path.join(process.cwd() + '/' + appname)
     })
